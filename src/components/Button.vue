088ab186ba08af7d29c841a5b64f45067ddac087
Anderson Bauermann Feltes <anderson.bauermannfeltes@gmail.com>
2024-01-29 19:11:05 -0300

    created button and display component

---
+++ src/components/Button.vue
@@ -0,0 +1,16 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <button class="button">
+        0
+    </button>
+</template>
+
+<script>
+export default {
+   
+}
+</script>
+
+<style>
+
+</style>
